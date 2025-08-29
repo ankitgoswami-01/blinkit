eb619af312e2a53d1cb29894f2e8cc38312d17c3
ankitgoswami-01 <ankitgirigoswami5678@gmail.com>
2025-08-29 16:03:51 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,120 @@
+
+    document.addEventListener('DOMContentLoaded', function () {
+      new Splide('#splide', {
+        type: 'loop',
+        perPage: 6,
+        perMove: 1,
+        autoplay: true,
+        // interval: 3000,
+        gap: '10px',
+        pagination: false,
+        breakpoints: {
+          1280: { perPage: 5 }, 
+          1024: { perPage: 3 },
+          768: { perPage: 3 },
+          480: { perPage: 1 },
+        },
+      }).mount();
+    });
+     document.addEventListener('DOMContentLoaded', function () {
+      new Splide('#splide1', {
+        type: 'loop',
+        perPage: 6,
+        perMove: 1,
+        autoplay: true,
+        // interval: 3000,
+        gap: '10px',
+        pagination: false,
+        breakpoints: {
+          1280: { perPage: 5 },
+          1024: { perPage: 3 },
+          768: { perPage: 3 },
+          480: { perPage: 1  },
+        },
+      }).mount();
+    });
+    document.addEventListener('DOMContentLoaded', function () {
+      new Splide('#splide2', {
+        type: 'loop',
+        perPage: 6,
+        perMove: 1,
+        autoplay: true,
+        // interval: 3000,
+        gap: '10px',
+        pagination: false,
+        breakpoints: {
+          1280: { perPage: 5 },
+          1024: { perPage: 3 },
+          768: { perPage: 3 },
+          480: { perPage: 1  },
+        },
+      }).mount();
+    });
+      document.addEventListener('DOMContentLoaded', function () {
+      new Splide('#splide3', {
+        type: 'loop',
+        perPage: 6,
+        perMove: 1,
+        autoplay: true,
+        // interval: 3000,
+        gap: '10px',
+        pagination: false,
+        breakpoints: {
+          1280: { perPage: 5 },
+          1024: { perPage: 3 },
+          768: { perPage: 3 },
+          480: { perPage: 1  },
+        },
+      }).mount();
+    });
+        document.addEventListener('DOMContentLoaded', function () {
+      new Splide('#splide4', {
+        type: 'loop',
+        perPage: 6,
+        perMove: 1,
+        autoplay: true,
+        // interval: 3000,
+        gap: '10px',
+        pagination: false,
+        breakpoints: {
+          1280: { perPage: 5 },
+          1024: { perPage: 3 },
+          768: { perPage: 3 },
+          480: { perPage: 1  },
+        },
+      }).mount();
+    });
+       document.addEventListener('DOMContentLoaded', function () {
+      new Splide('#splide5', {
+        type: 'loop',
+        perPage: 6,
+        perMove: 1,
+        autoplay: true,
+        // interval: 3000,
+        gap: '10px',
+        pagination: false,
+        breakpoints: {
+          1280: { perPage: 5 }, 
+          1024: { perPage: 3 },
+          768: { perPage: 3 },
+          480: { perPage: 1  },
+        },
+      }).mount();
+    });
+      document.addEventListener('DOMContentLoaded', function () {
+      new Splide('#splide6', {
+        type: 'loop',
+        perPage: 6,
+        perMove: 1,
+        autoplay: true,
+        // interval: 3000,
+        gap: '10px',
+        pagination: false,
+        breakpoints: {
+          1280: { perPage: 5 }, 
+          1024: { perPage: 3 },
+          768: { perPage: 3 },
+          480: { perPage: 1  },
+        },
+      }).mount();
+    });
